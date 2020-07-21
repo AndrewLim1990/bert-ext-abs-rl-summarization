6bddbab4e05ec2f694470a913e299b92b6c246d2
AndrewLim1990 <andrewlim90@gmail.com>
2020-07-20 22:05:10 -0700

    Implement extractor

---
+++ extractor/utils.py
@@ -0,0 +1,68 @@
+from torch import nn
+
+import torch
+
+BERT_OUTPUT_SIZE = 768
+BI_LSTM_OUTPUT_SIZE = 2
+
+
+class ExtractorModel(nn.Module):
+    def __init__(self, bi_lstm, ptr_lstm):
+        super(ExtractorModel, self).__init__()
+        self.bi_lstm = bi_lstm
+        self.ptr_lstm = ptr_lstm
+        self.softmax = nn.Softmax(dim=1)
+
+        # Todo: Find suitable attn_dim
+        attn_dim = self.ptr_lstm.hidden_size
+        self.linear_h = torch.nn.Linear(self.ptr_lstm.hidden_size, attn_dim, bias=False)
+        self.linear_e = torch.nn.Linear(self.ptr_lstm.hidden_size, attn_dim, bias=False)
+        self.linear_v = torch.nn.Linear(attn_dim, 1, bias=False)
+        self.tanh = torch.nn.Tanh()
+
+    def forward(self, input_embeddings):
+        """
+        Todo: Convert from "dot" attention mechanism to "additive" to match paper
+        Todo Ref: http://web.stanford.edu/class/cs224n/slides/cs224n-2020-lecture08-nmt.pdf
+        Todo: Provide better comments than labeling Eq numbers from paper
+
+        :param input_embeddings:
+        :return:
+        """
+        h, __ = self.bi_lstm(input_embeddings)
+        z, __ = self.ptr_lstm(h)
+        h, z = h.squeeze(), z.squeeze()
+
+        # Eq (3)
+        attn = torch.mm(h, z.T)  # Dot attention mechanism
+
+        # Eq (4)
+        attn_weights = self.softmax(attn)
+
+        # Eq (5)
+        e = torch.mm(attn_weights, h)
+
+        # Eq (1)
+        u = self.linear_h(h) + self.linear_e(e)
+        u = self.tanh(u)
+        u = self.linear_v(u).T
+
+        # Eq (2)
+        p = self.softmax(u)
+
+        return p
+
+
+bidirectional_lstm = nn.LSTM(
+    input_size=BERT_OUTPUT_SIZE,
+    hidden_size=BI_LSTM_OUTPUT_SIZE,
+    num_layers=1,
+    bidirectional=True
+)
+
+pointer_lstm = nn.LSTM(
+    input_size=BI_LSTM_OUTPUT_SIZE * 2,
+    hidden_size=BI_LSTM_OUTPUT_SIZE * 2,
+    num_layers=1,
+    bidirectional=False
+)
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from bert.utils import bert_model
 from bert.utils import bert_tokenizer
 from bert.utils import obtain_sentence_embeddings
+from extractor.utils import bidirectional_lstm
+from extractor.utils import pointer_lstm
+from extractor.utils import ExtractorModel
 from utils import euclidean_distance
 
 
@@ -11,10 +14,16 @@ def main():
         'two plus two is four'
     ]
     sentence_embeddings = obtain_sentence_embeddings(bert_model, bert_tokenizer, input_sentences)
+    sentence_embeddings = sentence_embeddings.unsqueeze(1)  # [n_sentences, n_batches, bert_output_dim]
 
-    for sentence, embedding in zip(input_sentences, sentence_embeddings):
-        dist = euclidean_distance(sentence_embeddings[0], embedding)
-        print(f"'{input_sentences[0]}' to '{sentence}' distance: {dist}")
+    # for sentence, embedding in zip(input_sentences, sentence_embeddings):
+    #     dist = euclidean_distance(sentence_embeddings[0], embedding)
+    #     print(f"'{input_sentences[0]}' to '{sentence}' distance: {dist}")
+    #
+    # print("\n")
+
+    extractor_model = ExtractorModel(bidirectional_lstm, pointer_lstm)
+    print("Result: {}".format(extractor_model(sentence_embeddings)))
 
 
 if __name__ == '__main__':
