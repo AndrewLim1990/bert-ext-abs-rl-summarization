3c40e81672bf7255eebbdd106e4958f3296033ab
AndrewLim1990 <andrewlim90@gmail.com>
2020-07-20 22:09:30 -0700

    Add detail to print statements

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ def main():
     sentence_embeddings = obtain_sentence_embeddings(bert_model, bert_tokenizer, input_sentences)
     sentence_embeddings = sentence_embeddings.unsqueeze(1)  # [n_sentences, n_batches, bert_output_dim]
 
-    # for sentence, embedding in zip(input_sentences, sentence_embeddings):
-    #     dist = euclidean_distance(sentence_embeddings[0], embedding)
-    #     print(f"'{input_sentences[0]}' to '{sentence}' distance: {dist}")
-    #
-    # print("\n")
+    for sentence, embedding in zip(input_sentences, sentence_embeddings):
+        dist = euclidean_distance(sentence_embeddings[0], embedding)
+        print(f"'{input_sentences[0]}' to '{sentence}' distance: {dist}")
+
+    print("\n")
 
     extractor_model = ExtractorModel(bidirectional_lstm, pointer_lstm)
-    print("Result: {}".format(extractor_model(sentence_embeddings)))
+    print("Extraction probability distribution amongst sentences: {}".format(extractor_model(sentence_embeddings)))
 
 
 if __name__ == '__main__':
