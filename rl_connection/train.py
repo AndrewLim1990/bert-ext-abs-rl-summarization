867e791c3726d01cfabeca28d227b0f397237e19
AndrewLim1990 <andrewlim90@gmail.com>
2020-08-28 14:01:48 -0700

    Start refactor

--- rl_connection/train.py
+++ rl_connection/train.py
@@ -31,22 +31,25 @@ def train_rl(rl_model, data, n_iters=10000):
         )
 
         # Run trajectory
-        actions, log_probs, entropys, values = rl_model.sample_actions(source_sentence_embeddings, source_mask)
+        actions, log_probs, entropys, values, n_ext_sents = rl_model.sample_actions(
+            source_sentence_embeddings,
+            source_mask
+        )
 
         # Obtain abstracted sentence from abstractor
         predicted_tokens, word_probabilities = rl_model.create_abstracted_sentences(
             actions,
             source_documents,
-            stop_action_index,
+            n_ext_sents=n_ext_sents,
             teacher_forcing_pct=1.0,
             target_summary_embeddings=target_summary_embeddings
         )
 
         # Obtain returns from ROUGE
-        rewards = rl_model.determine_rewards(actions, predicted_tokens, target_tokens, target_mask)
+        rewards = rl_model.determine_rewards(n_ext_sents, predicted_tokens, target_tokens, target_mask)
 
         # Update RL model
-        last_action_mask = rl_model.last_action_mask(actions)
+        last_action_mask = rl_model.last_action_mask(actions, n_ext_sents)
 
         # Calc trajectories
         trajectories = list(zip(actions, rewards, log_probs, entropys, values, last_action_mask))
@@ -63,7 +66,7 @@ def get_training_batch(training_dictionaries, batch_size):
     :param batch_size:
     :return:
     """
-    mini_batch = np.random.choice(training_dictionaries, batch_size).tolist()
+    mini_batch = training_dictionaries[:batch_size]  # np.random.choice(training_dictionaries, batch_size).tolist()
 
     documents, summaries = map(
         list,
--- utils.py
+++ utils.py
@@ -48,3 +48,18 @@ def compute_rouge_l(output, reference, mode='f'):
         else:
             score = f_score
     return score
+
+
+def batched_index_select(input, dim, index):
+    """
+    :param input:
+    :param dim:
+    :param index:
+    :return:
+    """
+    views = [input.shape[0]] + [1 if i != dim else -1 for i in range(1, len(input.shape))]
+    expanse = list(input.shape)
+    expanse[0] = -1
+    expanse[dim] = -1
+    index = index.view(views).expand(expanse)
+    return torch.gather(input, dim, index)
